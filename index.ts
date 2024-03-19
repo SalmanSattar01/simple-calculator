9fbe87c90a88c328314c047edd6e893c228e32a3
salman sattar <salmansattar01@gmail.com>
2024-03-20 00:02:12 +0500

    code edited

--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
 import inquirer from "inquirer";
 
-const answer = await inquirer.prompt([{message:}])
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction","Multiplication", "Division"],
+  },
+]);
+
+//conditional statement
+if(answer.operator === "Addition"){
+    console.log(`your value is ${answer.firstNumber + answer.secondNumber}`);
+} else if(answer.operator === "Subtraction"){
+    console.log(`your value is ${answer.firstNumber - answer.secondNumber}`); 
+} else if(answer.operator === "Multiplication"){
+    console.log(`your value is ${answer.firstNumber * answer.secondNumber}`); 
+} else if(answer.operator === "Division"){
+    console.log(`your value is ${answer.firstNumber / answer.secondNumber}`); 
+} else {
+    console.log("Please Select a valid operator")
+}
